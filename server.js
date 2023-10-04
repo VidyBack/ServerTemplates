e4a8fc0c6c5ae6120fb17741fb6693e8389ea46c
Junaidkhan1809 <72540582+Junaidkhan1809@users.noreply.github.com>
2023-10-04 13:01:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const data = require('./db.json')
 const middlewares = jsonServer.defaults();
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
@@ -13,17 +14,23 @@ server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 server.post('/your-endpoint', (req, res) => {
-  // Process the request body and add/update data in db.json using lowdb
-  // For example:
-  const newData = req.body;
- // Add a new post
-db.get('posts').push(newData).write();
+    // Process the request body and add/update data in db.json using lowdb
+    // For example:
+    const newData = req.body;
+   // Add a new post
+  db.get('posts').push(newData).write();
+  
+  console.log('Added new post:', newData);
+  
+  
+    res.status(200).json(newData);
+  });
 
-console.log('Added new post:', newData);
-
-
-  res.status(200).json(newData);
-});
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
 
 server.use(router);
 
